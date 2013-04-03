79d7a0a8b4b04927cb002577c08bb15230ba39f5
Vipin Nair <swvist@gmail.com>
2013-04-04 02:30:39 +0530

    Initializing Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveyc::Application.config.secret_token = '340ba62184d583c0f71992cd18a5a42789cafafb9714bac11e713958a513d41d507a37e648c2ee15f316c892e5d9788acf0035dbcdffce5c2ec414db5482620c'
