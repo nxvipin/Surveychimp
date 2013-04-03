39afee603315765648bb866b4c6890cb899db378
Vipin Nair <swvist@gmail.com>
2013-04-04 03:19:14 +0530

    Adding a new functional test for survey controller. Updating to use the current fixtures

--- test/functional/questions_controller_test.rb
+++ test/functional/questions_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
   setup do
-    @question = questions(:one)
+    @question = questions(:question_valid)
+    @question_fail = questions(:question_invalid_survey_id)
   end
 
   test "should get index" do
@@ -20,9 +21,16 @@ class QuestionsControllerTest < ActionController::TestCase
     assert_difference('Question.count') do
       post :create, question: { question: @question.question, question_options: @question.question_options, question_type: @question.question_type, survey_id: @question.survey_id }
     end
-
     assert_redirected_to question_path(assigns(:question))
   end
+
+test "should not create question" do
+    assert_no_difference('Question.count') do
+      post :create, question: { question: @question_fail.question, question_options: @question_fail.question_options, question_type: @question_fail.question_type, survey_id: @question_fail.survey_id }
+    end
+    assert_select "div#error_explanation ul li", "Survey does not exist"
+  end
+
 
   test "should show question" do
     get :show, id: @question
