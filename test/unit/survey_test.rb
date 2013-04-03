3ce9fb21881095fda8ac1a1e85ef3e13ed26ce7c
Vipin Nair <swvist@gmail.com>
2013-04-04 03:25:47 +0530

    Adding unit test for surveys

--- test/unit/survey_test.rb
+++ test/unit/survey_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class SurveyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Survey should be added" do
+    survey = surveys(:survey_valid)
+    assert survey.valid?
+  end
+  
+  test "Survey attributes must not be empty" do
+    survey = Survey.new
+    assert survey.invalid?
+    assert survey.errors[:title].any?
+    assert survey.errors[:description].any?
+  end
+
 end
