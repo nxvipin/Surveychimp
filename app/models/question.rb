f426b450401c242713022d4faeeb73029b5b9cc4
Vipin Nair <swvist@gmail.com>
2013-04-04 03:08:02 +0530

    Adding basic validations to questions model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,12 @@
 class Question < ActiveRecord::Base
   attr_accessible :question, :question_options, :question_type, :survey_id
+  belongs_to :survey
+  validates :question, :presence => true
+  validates :question_type, :presence => true
+  validates :question_options, :presence => true
+  validates :survey_id, :existence => true
+  validates :question_type,
+    :inclusion => {
+      :in => %w(numeric essay date),
+      :message => "Not valid type" }
 end
