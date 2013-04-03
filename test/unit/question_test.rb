73a7114104f60db24049562768e7abc25d62deec
Vipin Nair <swvist@gmail.com>
2013-04-04 03:25:27 +0530

    Adding unit test for questions

--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "Question should be added" do
+    question = questions(:question_valid)
+    assert question.valid?
+  end
+  
+  test "Question attributes must not be empty" do
+    question = Question.new
+    assert question.invalid?
+    assert question.errors[:question].any?
+    assert question.errors[:question_type].any?
+    assert question.errors[:question_options].any?
+    assert question.errors[:survey_id].any?
+  end
+
+  test "survey_id of the question must exist" do
+    question = questions(:question_invalid_survey_id)
+    assert question.invalid?
+    assert question.errors[:survey_id], "does not exist"
+  end
+
+  test "question type must be either 'numeric' or 'essay' or 'date'" do
+    question = questions(:question_invalid_type)
+    assert question.invalid?
+  end
+
 end
