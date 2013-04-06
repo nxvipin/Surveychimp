15d155cd21ad7b96a4c51b3419c33ea6ed2bbb1d
Vipin Nair <swvist@gmail.com>
2013-04-06 16:00:39 +0530

    Making nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Surveyc::Application.routes.draw do
-  resources :questions
 
-
-  resources :surveys
+  resources :surveys do 
+    resources :questions
+  end
 
 
   # The priority is based upon order of creation:
